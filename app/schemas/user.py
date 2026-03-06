97102a80f1560bd7dc5557c3a509a2564f04212b
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-03-06 16:54:30 +0500

    feat(schemas): user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class UserBase(BaseModel):
+    email: EmailStr
+    first_name: str
+    last_name: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    first_name: str | None = None  # Все опционально
+    last_name: str | None = None
+    password: str | None = None
+
+
+class UserResponse(UserBase):
+    id: int
+    is_active: bool
+    is_admin: bool
+    created_at: datetime
+
+    # from_attributes: True нужен только в схемах которые ты возвращаешь из роутера (Response схемы)
+    # Что бы Pydantic умел читать не только ключи словаря, но и атрибуты объекта
+    model_config = {"from_attributes": True}
