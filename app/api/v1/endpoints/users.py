8e30d0b1e5dadc0ebd1c0c83333557440ae40f89
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-03-19 17:31:41 +0500

    feat(endpoints): защитил эндпоинты get_user и get_all_users в users.py, написал эндпоинт get_me

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.api.dependencies import get_current_user, get_current_admin
 from app.db.sesion import get_db
+from app.models import User
 from app.schemas.user import UserCreate, UserResponse
 from app.services.user import UserService
 
@@ -13,14 +15,24 @@ async def register(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
     service = UserService(db)
     return await service.register(user_data)
 
+
+@router.get("/me", response_model=UserResponse)
+async def get_me(current_user: User = Depends(get_current_user)):
+    """Возвращает профиль только текущего авторизованного пользователя."""
+    return UserResponse.model_validate(current_user)
+
+
 @router.get("/{user_id}", response_model=UserResponse)
-async def get_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    """Возвращает пользователя по id. 404 если не найден."""
+async def get_user(user_id: int, db: AsyncSession = Depends(get_db),
+                   current_user: User = Depends(get_current_admin)):
+    """Возвращает пользователя по id только для админов. 404 если не найден."""
     service = UserService(db)
     return await service.get_by_id(user_id)
 
+
 @router.get("/", response_model=list[UserResponse])
-async def get_all_users(db: AsyncSession = Depends(get_db)):
-    """Возвращает список всех пользователей."""
+async def get_all_users(db: AsyncSession = Depends(get_db),
+                        current_user: User = Depends(get_current_admin)):
+    """Возвращает список всех пользователей только для админов."""
     service = UserService(db)
     return await service.get_all()
