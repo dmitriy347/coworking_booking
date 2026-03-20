ac0ccc81ed1c06689644b0ea9b392f90d019fffc
Dmitriy Fomenko <d.fomenko95@gmail.com>
2026-03-20 16:06:17 +0500

    feat(app/models): добавил workspace.py

---
+++ app/models/workspace.py
@@ -0,0 +1,20 @@
+from sqlalchemy import String, Text, Numeric, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.models.base import Base
+from app.models.enum import WorkspaceType
+import sqlalchemy as sa
+
+
+class Workspace(Base):
+    """Модель рабочего места."""
+    __tablename__ = "workspaces"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=False)
+    type: Mapped[WorkspaceType] = mapped_column(sa.Enum(WorkspaceType), name="workspacetype", nullable=False)
+    price_per_hour: Mapped[float] = mapped_column(Numeric(10,2), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    # Cвязь с бронированием
+    bookings: Mapped[list["Booking"]] = relationship(back_populates="workspace")
